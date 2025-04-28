7e20d7b879b8968f7b6b8dbe2fa41469abfd3bcc
pawelvds <karczewskipawel.j@gmail.com>
2025-04-29 00:33:43 +0200

    Added refresh tokens solution

--- DTOs/Auth/AuthResponse.cs
+++ DTOs/Auth/AuthResponse.cs
@@ -4,4 +4,5 @@ public class AuthResponse
 {
     public string Token { get; set; } = string.Empty;
     public DateTime Expiration { get; set; }
+    public string RefreshToken { get; set; } = string.Empty;
 }
---
+++ Models/RefreshToken.cs
@@ -0,0 +1,10 @@
+﻿namespace InsightBoard.Api.Models;
+
+public class RefreshToken
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string UserId { get; set; } = string.Empty;
+    public User User { get; set; } = null!;
+    public DateTime ExpiresAt { get; set; }
+    public bool IsRevoked { get; set; } = false;
+}
