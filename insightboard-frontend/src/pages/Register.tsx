f0ba345402f03090b32660eff4648d1a165dd00a
pawelvds <karczewskipawel.j@gmail.com>
2025-04-29 12:42:15 +0200

    Added loging and register

---
+++ insightboard-frontend/src/pages/Register.tsx
@@ -0,0 +1,29 @@
+﻿function Register() {
+    return (
+        <div className="flex justify-center items-center h-screen bg-gray-50">
+            <div className="bg-white p-8 rounded shadow w-full max-w-sm">
+                <h2 className="text-2xl font-bold mb-4 text-center">Register</h2>
+                <form className="space-y-4">
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        className="w-full p-2 border rounded"
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        className="w-full p-2 border rounded"
+                    />
+                    <button
+                        type="submit"
+                        className="w-full bg-blue-600 text-white p-2 rounded hover:bg-blue-700"
+                    >
+                        Sign In
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Register
