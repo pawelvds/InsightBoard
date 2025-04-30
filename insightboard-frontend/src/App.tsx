d073d245f9aeab63840ca9be4c1ae2179c9b37b5
pawelvds <karczewskipawel.j@gmail.com>
2025-04-30 13:58:30 +0200

    shadcn configuration

---
+++ insightboard-frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="*" element={<div className="p-4 text-center">404 Not Found</div>} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default App
